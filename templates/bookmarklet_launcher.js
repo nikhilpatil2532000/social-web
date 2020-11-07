6bdbb2a8813f2891fbc12b4900135beef69feae0
nikhilpatil2532000 <69834562+nikhilpatil2532000@users.noreply.github.com>
2020-11-07 13:38:32 +0530

    Update bookmarklet_launcher.js

--- templates/bookmarklet_launcher.js
+++ templates/bookmarklet_launcher.js
@@ -3,7 +3,7 @@
         myBookmarklet();
     }
     else {
-        document.body.appendChild(document.createElement('script')).src = 'http://1f7178c3bb60.ngrok.io/static/js/bookmarklet.js ?r='+Math.floor(Math.random()*99999999999999999999);
+        document.body.appendChild(document.createElement('script')).src = 'http://1e2b5b3c007c.ngrok.io/static/js/bookmarklet.js ?r='+Math.floor(Math.random()*99999999999999999999);
 			}
 })();
 
